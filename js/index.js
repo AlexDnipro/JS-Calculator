b77f0fe171e499d102312e60246046639ac0e448
AlexDnipro <olexiyusov@gmail.com>
2020-02-12 17:57:39 +0000

    Release of basic calculator 1.0 without memory functions

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
-//function universe() {
+  (function universe() { //Parent function to contain our global variables;
   const display = document.querySelector(".display");
 
-  //var memoryStore;
-  var currentOperation;
-  var cloudValue;
-  var displayImage;
-  //var newValue;
+  var currentOperation; //Stores the last operation button that we pressed
+  var displayImage = ""; //Our virtual calculation environment is initialized with empty string
+
 
   document.querySelector(".clear").addEventListener("click", clearDisplay);
 
-  function clearDisplay() {
-    cloudValue = "";
+  function clearDisplay() { //Clear button function - cleans client display and all auxilary variables
     display.value = "";
     currentOperation = "";
     displayImage = "";
@@ -18,66 +15,45 @@
 
   document.querySelectorAll(".digits button").forEach(button => button.addEventListener("click", digitPressed));
 
-  function digitPressed(ev) {
-    //console.log(ev.target.innerText);
-    //display.value = ev.target.innerText;
-    display.value += ev.target.innerText;
-    cloudValue = display.value;
+  function digitPressed(ev) { //Function that works with digit buttons
+    if (displayImage === "" && ev.target.innerText === ".") { //When we try to press "." with empty calculator it will return "0."
+        display.value = "0.";
+        displayImage = display.value;
+    } else if ( display.value.slice(-1) === "." && ev.target.innerText === "." ) { //This will block input of repeating float signs
+        display.value = displayImage;
+    } else {
+        display.value = displayImage + ev.target.innerText;
+        displayImage = display.value;
+    }
+    
     }
 
   document.querySelectorAll(".opers button").forEach(button => button.addEventListener("click", opersPressed));
 
-  function opersPressed(ev) {
-    
-    //currentOperation = ev.target.innerText;
-    //if (currentOperation != ev.target.innerText) {
-    currentOperation = ev.target.innerText;
-    display.value = cloudValue + currentOperation;
-    displayImage = display.value;
-    //display.value += ev.target.innerText;
-    
+  function opersPressed(ev) { //Function that works with operation buttons
+    if (display.value === "") {
+        currentOperation = ev.target.innerText;
+        display.value = displayImage;
+    } else {
+        currentOperation = ev.target.innerText;
+        display.value = display.value + currentOperation;
+        displayImage = display.value;
+    }
   }
 
   document.querySelector(".equal").addEventListener("click", calculus);
 
-  function calculus() { 
-    //   if (displayImage.charAt(-1) === currentOperation) { 
-    //     display.value = "finished with operator";
-    //    } else {
-    //     display.value = eval(displayImage);
-    //    }  
+  function calculus() {
     displayImage = display.value;
-    if (display.value.slice(-1) === currentOperation) {
-        //displayImage = displayImage + 
-        display.value = 'hello';
-       } else {
+    if (display.value.slice(-1) === currentOperation) { //If we have unfinished operation as for example "n+" in the display
+        displayImage = displayImage + displayImage.slice(0, displayImage.length - 1); //it will automatically add "n" to it and return "n+n".
+        display.value = eval(displayImage); // I could convert to numbers but I decided to experiment with eval.
+        displayImage = "";
+        
+    } else {
         display.value = eval(displayImage);
-        cloudValue = display.value;
+        displayImage = ""; //Clean our virtual calculation environment after calculus function called.
        } 
     }
       
-//   document.querySelector(".memplus").addEventListener("click", memAdd);
-
-//   function memAdd() {
-//     //display.value += ev.target.innerText;
-//     memoryStore = memoryStore + parseInt(display.value);
-//   }
-
-  //document.querySelector(".memdeduct").addEventListener("click", memSubstract);
-
-//   function memSubstract() {
-//     memoryStore = memoryStore - parseInt(display.value);
-//   }
-
-//   document.querySelector(".memdisplay").addEventListener("click", memShow);
-
-//   function memShow() {
-//     display.value = memoryStore.toString();
-//   }
-
-//   document.querySelector(".memclear").addEventListener("click", memClean);
-
-//   function memClean() {
-//     memoryStore = 0;
-//   }
-//}
+})();
