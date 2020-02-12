338a62d90286195be84d508bb26c95642925c5d7
AlexDnipro <olexiyusov@gmail.com>
2020-02-12 14:17:57 +0000

    working on calculus function

--- js/index.js
+++ js/index.js
@@ -1,56 +1,82 @@
-const display = document.querySelector('.display');
+//function universe() {
+  const display = document.querySelector(".display");
 
-let memoryStore = 0;
+  //var memoryStore;
+  var currentOperation;
+  var cloudValue;
+  var displayImage;
+  //var newValue;
 
-document.querySelector('.clear').addEventListener('click', clearDisplay);
+  document.querySelector(".clear").addEventListener("click", clearDisplay);
 
-function clearDisplay () {
+  function clearDisplay() {
+    cloudValue = "";
     display.value = "";
-}
+    currentOperation = "";
+    displayImage = "";
+  }
 
-document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+  document.querySelectorAll(".digits button").forEach(button => button.addEventListener("click", digitPressed));
 
-function digitPressed(ev) {
-   //display.value = ""; // Trying to clear screen after working with M+
+  function digitPressed(ev) {
+    //console.log(ev.target.innerText);
+    //display.value = ev.target.innerText;
     display.value += ev.target.innerText;
-}
+    cloudValue = display.value;
+    }
 
-document.querySelectorAll('.opers button')
-    .forEach( button => button.addEventListener('click', opersPressed));
+  document.querySelectorAll(".opers button").forEach(button => button.addEventListener("click", opersPressed));
 
-function opersPressed(ev) {
-    display.value += ev.target.innerText;
-}
-
-document.querySelector('.equal').addEventListener("click", result);
-
-function result() {
-    display.value = eval(display.value);
-}
-
-document.querySelector('.memplus').addEventListener("click", memAdd);
-
-function memAdd() {
+  function opersPressed(ev) {
+    
+    //currentOperation = ev.target.innerText;
+    //if (currentOperation != ev.target.innerText) {
+    currentOperation = ev.target.innerText;
+    display.value = cloudValue + currentOperation;
+    //displayImage = display.value;
     //display.value += ev.target.innerText;
-    memoryStore = memoryStore + parseInt(display.value);
-}
+    
+  }
 
-document.querySelector('.memdeduct').addEventListener("click", memSubstract);
+  document.querySelector(".equal").addEventListener("click", calculus);
 
-function memSubstract() {
-    memoryStore = memoryStore - parseInt(display.value);
-}
+  function calculus() { 
+    //   if (displayImage.charAt(-1) === currentOperation) { 
+    //     display.value = "finished with operator";
+    //    } else {
+    //     display.value = eval(displayImage);
+    //    }  
+    displayImage = display.value;
+    if (displayImage.charAt(-1) === '+') {
+        display.value = 'hello';
+       } else {
+        display.value = eval(display.value);
+        cloudValue = display.value;
+              }
+    }
+      
+//   document.querySelector(".memplus").addEventListener("click", memAdd);
 
-document.querySelector('.memdisplay').addEventListener("click", memShow);
+//   function memAdd() {
+//     //display.value += ev.target.innerText;
+//     memoryStore = memoryStore + parseInt(display.value);
+//   }
 
-function memShow() {
-    display.value = memoryStore.toString();
-}
+  //document.querySelector(".memdeduct").addEventListener("click", memSubstract);
 
-document.querySelector('.memclear').addEventListener("click", memClean);
+//   function memSubstract() {
+//     memoryStore = memoryStore - parseInt(display.value);
+//   }
 
-function memClean() {
-    memoryStore = 0;
-}
+//   document.querySelector(".memdisplay").addEventListener("click", memShow);
 
+//   function memShow() {
+//     display.value = memoryStore.toString();
+//   }
+
+//   document.querySelector(".memclear").addEventListener("click", memClean);
+
+//   function memClean() {
+//     memoryStore = 0;
+//   }
+//}
